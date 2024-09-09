843d6f418d00d6602c86774ab7f1e2a53b57048e
ghost640 <raddatz.b@stu.janesville.k12.wi.us>
2024-09-09 14:33:25 -0500

    update 9/9

---
+++ prog_58b.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+
+func _on_btn_calc_pressed() -> void:
+	var a = int($txtA.text)
+	var b = int($txtB.text)
+	var c = int($txtC.text)
+	var root = (-b + )
+
+func _on_btn_clear_pressed() -> void:
+	$txtA.text = ""
+	$txtB.text = ""
+	$txtC.text = ""
+	$lblRoot.text = ""
+
+
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
