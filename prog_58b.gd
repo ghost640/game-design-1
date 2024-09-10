feb5a7a0f983ab42f066d6911f72e947635437b6
ghost640 <raddatz.b@stu.janesville.k12.wi.us>
2024-09-10 14:23:32 -0500

    update 9/10

--- prog_58b.gd
+++ prog_58b.gd
@@ -6,7 +6,9 @@ func _on_btn_calc_pressed() -> void:
 	var a = int($txtA.text)
 	var b = int($txtB.text)
 	var c = int($txtC.text)
-	
+	var Root1 = (-b + sqrt(b * b - 4 * a * c)) / 2 * a
+	var Root2 = (-b - sqrt(b * b - 4 * a * c)) / 2 * a
+	$lblRoot.text = "the roots are: " + str(Root1) + "," + str(Root2)
 
 func _on_btn_clear_pressed() -> void:
 	$txtA.text = ""
