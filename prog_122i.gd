1a3dbfbeeb2347bda0af140b84e6cf1a82ebeccb
ghost640 <raddatz.b@stu.janesville.k12.wi.us>
2024-09-20 14:34:41 -0500

    update 9/20

---
+++ prog_122i.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+func _on_btn_calc_pressed() -> void:
+	$ItemList.add_item("number    cuberoot    cube")
+	for num in range(-25, 25+1):
+		var numcubed = num**3
+		var numcuberoot = num**.75
+		var line = "%d    %.4f    %d" % [num, numcuberoot, numcubed]
+		$ItemList.add_item(line)
+
+func _on_btn_clear_pressed() -> void:
+	$ItemList.clear()
+
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
