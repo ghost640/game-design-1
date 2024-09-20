6edf50e1b1a10ab4600200244a45029013f365fa
ghost640 <raddatz.b@stu.janesville.k12.wi.us>
2024-09-20 14:04:04 -0500

    update 9/20

--- prog_122b.gd
+++ prog_122b.gd
@@ -3,7 +3,11 @@ extends Control
 
 
 func _on_btn_calc_pressed() -> void:
-	$ItemList.add
+	$ItemList.add_item("hours    pay")
+	for num in range (1, 40+1):
+		var pay = num * 4
+		var line = "%d    %d" % [num, pay]
+		$ItemList.add_item(line)
 
 
 func _on_btn_clear_pressed() -> void:
