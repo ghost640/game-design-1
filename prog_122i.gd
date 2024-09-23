d68211791b005bccdeafb2706733917ef26b58cb
ghost640 <raddatz.b@stu.janesville.k12.wi.us>
2024-09-23 14:13:21 -0500

    update 9*/23

--- prog_122i.gd
+++ prog_122i.gd
@@ -6,12 +6,12 @@ func _on_btn_calc_pressed() -> void:
 	$ItemList.add_item("number    cuberoot    cube")
 	for num in range(-25, 25+1):
 		var numcubed = num**3
-		var numcuberoot = 
-		var line = "%d    %.4f    %d" % [num, numcuberoot, numcubed]
+		var numcuberoot = num**1.00/3
+		var line = "%d         %.4f        %d" % [num, numcuberoot, numcubed]
 		$ItemList.add_item(line)
 
 func _on_btn_clear_pressed() -> void:
 	$ItemList.clear()
 
 func _on_btn_exit_pressed() -> void:
-	geget_tree().quit()
+	get_tree().quit()
