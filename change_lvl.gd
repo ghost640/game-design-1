4393dd51b78250704c46f1123322651409312037
ghost640 <raddatz.b@stu.janesville.k12.wi.us>
2024-11-20 14:30:45 -0600

    update 11/20

--- change_lvl.gd
+++ change_lvl.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("Player"):
 		if next_level == "":
 			OS.alert("No next level!")
 		else:
