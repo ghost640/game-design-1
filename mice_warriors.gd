e11d752ca00d23f586272f895196af67ddf9c6c9
ghost640 <raddatz.b@stu.janesville.k12.wi.us>
2025-01-10 14:32:43 -0600

    update 1/10

---
+++ mice_warriors.gd
@@ -0,0 +1,27 @@
+extends RigidBody2D
+
+var speed = 100
+var direction = -1
+
+func _ready() -> void:
+	$Timer.start()
+
+func _physics_process(delta: float) -> void:
+	self.linear_velocity.x = speed * direction
+
+func _on_timer_timeout() -> void:
+	direction *= -1
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == "biscuit_the_warrior":
+		body.queue_free()
+		OS.alert("you died!")
+		get_tree().reload_current_scene()
+		
+	update_animation(direction)
+
+@onready var anim = $AnimatedSprite2D
+func update_animation(direction):
+	if direction != 0:
+		anim.play("walking")
+		anim.flip_h = direction < 0
